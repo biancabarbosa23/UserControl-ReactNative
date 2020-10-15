94f96777a72a70aeb435024f3ad52d5c444b4ff7
Bianca Alves <biancaa.barbosa@outlook.com>
2020-10-15 01:59:38 -0300

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,21 @@
-import React, { useState } from 'react'
-import { StyleSheet, View, Image, Animated } from 'react-native'
+import React from 'react'
+import { SafeAreaView, StyleSheet, View, Image } from 'react-native'
 
 export default function Header() {
-  const { scrollY, setScrollY } = useState(new Animated.Value(0))
-
   return (
-    <View style={styles.divLogo}>
-      <Image
-        source={require('../../../assets/Mind-Branco.png')}
-        style={{ width: 200, height: 55 }}
-        resizeMode="contain"
-      />
-    </View>
+    <SafeAreaView>
+      <View style={styles.divLogo}>
+        <Image
+          style={styles.logo}
+          source={require('../../../assets/Mind-Branco.png')}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   divLogo: {
-    flex: 1,
-    maxHeight: 70,
-    paddingTop: 15,
-    minWidth: '100%',
-    alignItems: 'center',
     backgroundColor: '#191919',
-    padding: 6,
   },
 })
